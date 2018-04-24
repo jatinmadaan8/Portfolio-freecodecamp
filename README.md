86e2939a1d8971f8fe3d672f88e5e6cc208014ec
jatinmadaan8 <jatinmadaan008@gmail.com>
2018-04-24 20:31:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio #
+A Pen created at CodePen.io. You can find this one at https://codepen.io/jatin008/pen/JyaGRZ.
+
+ My portfolio
---
+++ js/index.js
@@ -0,0 +1,38 @@
+$('a[href^="#hublyn"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+$('a[href^="#portfolio"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+$('a[href^="#contact"]').on('click', function(event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
